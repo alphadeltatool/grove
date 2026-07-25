236fe3fe690e76ccc5d7385fbdba9200cb6b8b3b
남용수 <maggapass@gmail.com>
2026-07-25 22:00:23 +0900

    feat(ds): implement MyArrayList with dynamic resize
    - Object[] 기반 동적 배열 직접 구현 (add, grow)
    - grow 1.5배 + System.arraycopy로 resize
    - Day 1 백지인출 노트 정리 (연속 메모리, size 변수, amortized O(1))

---
+++ data-structure/MyArrayList.java
@@ -0,0 +1,35 @@
+
+
+public class MyArrayList {
+    private Object[] elementData;
+    private int size;
+
+    public MyArrayList() {
+        elementData = new Object[5];
+    }
+
+    public void add(Object value) {
+        if (size == elementData.length) {
+            grow();
+        }
+
+        elementData[size] = value;
+        size++;
+    }
+
+    private void grow() {
+        int newCapacity = elementData.length + (elementData.length/2);
+        Object[] newArray = new Object[newCapacity];
+        System.arraycopy(elementData, 0, newArray, 0, size);
+        elementData = newArray;
+        System.out.println("Array grown to new capacity: " + newCapacity);
+    }
+
+    public static void main(String[] args) {
+        MyArrayList list = new MyArrayList();
+        for(int i = 0; i < 11; i ++ ) {
+            list.add(i);
+            System.out.println("Added: " + i);
+        }
+    }
+}
